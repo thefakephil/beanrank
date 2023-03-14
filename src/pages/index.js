59c84586c98fdace21bca509b2f80d686d0533cc
Phil Gebauer <philgebauer@Phils-Laptop.local>
2023-03-14 12:36:22 -0500

    update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Beanrank - its for beans</title>
         <meta name="description" content="Beanrank - its for beans" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/beans.png" />
